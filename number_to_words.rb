766fecc0361d4e56df37f7a8f8d345c0ff079bae
Lyra-B <glykeriapeppa@gmail.com>
2015-01-20 21:01:18 +0000

    Tests passed for 1-100

--- number_to_words.rb
+++ number_to_words.rb
@@ -1,8 +1,87 @@
+require 'pry'
+
 module NumberToWords
   def to_words(number = self)
+  	nums_to_words = ['', 'one','two','three','four', 'five', 'six', 'seven', 
+		'eight', 'nine', 'ten','eleven','twelve','thirteen','fourteen', 
+		'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+		one_to_ten_in_words = ['','-one','-two','-three','-four', '-five', '-six', 
+  	'-seven', '-eight', '-nine']
+		tens_in_words = [ '','ten','twenty', 'thirty', 'forty', 'fifty', 'sixty', 
+		'seventy', 'eighty', 'ninety']
+    case number
+    	when 0..19
+    		nums = (0..19)
+				nums.map{|k| k.to_s.to_sym}
+				numbers_one_to_nineteen = Hash[nums.to_a.zip(nums_to_words)]
+				numbers_one_to_nineteen.each{|key,value| number = value if key==number}
+				number
+		  when 20..99
+				digits = number.to_s.split('').map{|k| k.to_i}
+				#binding.pry
+				if digits[1] == 0
+				 	number = tens_in_words[digits[0]]
+				else
+					digits[0] = tens_in_words[digits[0]]
+					digits[1] = one_to_ten_in_words[digits[1]]
+					number = "#{digits[0]}#{digits[1]}"
+				end
+      when 100..999
+      	digits = number.to_s.split('').map{|k| k.to_i}
+      	if digits[1] == 1
+      		"#{nums_to_words[digits[0]]} hundred #{nums_to_words[digits[2]-10]}"
+      	elsif digits[1] == 0
+      		"#{nums_to_words[digits[0]]} hundred #{nums_to_words[digits[2]]}".strip
+      	# if digits[1]==0 && digits[2]==0
+      	# 	number = "#{nums_to_words[digits[0]]} hundred"
+      	# elsif digits[2] == 0
+      	#   number = 
+      	#   "#{nums_to_words[digits[0]]} hundred #{tens_in_words[digits[1]-1]}"
+      	# elsif digits[1] == 0
+      	# 	number = 
+      	# 	"#{nums_to_words[digits[0]]} hundred #{one_to_ten_in_words[digits[2]].delete('-')}" 
+      	else
+      		number = 
+      		"#{nums_to_words[digits[0]]} hundred #{tens_in_words[digits[1]]}#{one_to_ten_in_words[digits[2]]}".strip
+      	end
+
+      when 1000..9999
+				digits = number.to_s.split('').map{|k| k.to_i}
+				if digits[1]==0
+					number = "#{nums_to_words[digits[0]]} thousand #{tens_in_words[digits[2]]}#{one_to_ten_in_words[digits[3]]}".strip
+				elsif digits[2] == 0
+					number = "#{nums_to_words[digits[0]]} thousand #{nums_to_words[digits[1]]} hundred #{tens_in_words[digits[2]]}#{one_to_ten_in_words[digits[3]]}".strip
+				 # elsif digits[2] == 0
+					# number = "#{}"
+      # # 	if digits[1]==0 && digits[2]==0 && digits[3]==0
+      # # 		number = 
+      # # 		"#{nums_to_words[digits[0]]} thousand" 
+      # # 	elsif digits[2]==0 && digits[3] == 0
+      # # 		number = 
+      # # 		"#{nums_to_words[digits[0]]} thousand #{nums_to_words[digits[1]]} hundred"
+      # # 	elsif digits[1]==0 && digits[3] == 0
+      # # 		number = 
+      # # 		"#{nums_to_words[digits[0]]} thousand #{tens_in_words[digits[2]]}"
+      # # 	elsif digits[1] == 0
+      # # 		number = 
+      # # 		"#{nums_to_words[digits[0]]} thousand #{tens_in_words[digits[2]-1]}#{one_to_ten_in_words[digits[3]-1].delete('-')}"
+      # # 	elsif digits[2] == 0
+      # # 		number = 
+      # # 		"#{nums_to_words[digits[0]]} thousand #{nums_to_words[digits[1]]} hundred #{one_to_ten_in_words[digits[3]-1].delete('-')}"
+      # # 	elsif digits[3] == 0
+      # # 		number = 
+      # # 		"#{nums_to_words[digits[1]]} hundred #{tens_in_words[digits[2]-1]}"
+        else
+          number = "#{nums_to_words[digits[0]]} thousand #{nums_to_words[digits[1]]} hundred #{tens_in_words[digits[2]]}#{one_to_ten_in_words[digits[3]]}".strip
+        end
+    end
   end
 end
 
 class Fixnum
   include NumberToWords
 end
+
+
+
+
--- numbers_to_words_spec.rb
+++ numbers_to_words_spec.rb
@@ -10,5 +10,59 @@ class NumberToWordsTest < MiniTest::Test
       11.to_words.must_equal("eleven")
       12.to_words.must_equal("twelve")
     end
-  end
+
+    it "should handle the teens" do
+		  16.to_words.must_equal("sixteen")
+		  19.to_words.must_equal("nineteen")
+		end
+
+		it "should handle twenties" do
+		  30.to_words.must_equal("thirty")
+		  23.to_words.must_equal("twenty-three")
+		  26.to_words.must_equal("twenty-six")
+		  38.to_words.must_equal("thirty-eight")
+		  45.to_words.must_equal("forty-five")
+		  68.to_words.must_equal("sixty-eight")
+		  72.to_words.must_equal("seventy-two")
+		  84.to_words.must_equal("eighty-four")
+		  97.to_words.must_equal("ninety-seven")
+		end
+
+		it "should handle one-hundred to nine hundred ninety-nine" do	  
+		  120.to_words.must_equal("one hundred twenty")
+		  100.to_words.must_equal("one hundred")
+		  119.to_words.must_equal("one hundred nineteen")
+		  101.to_words.must_equal("one hundred one")
+		  200.to_words.must_equal("two hundred")
+		  235.to_words.must_equal("two hundred thirty-five")
+		  448.to_words.must_equal("four hundred forty-eight")
+		  569.to_words.must_equal("five hundred sixty-nine")
+		  378.to_words.must_equal("three hundred seventy-eight")
+		  789.to_words.must_equal("seven hundred eighty-nine")
+		  193.to_words.must_equal("one hundred ninety-three")
+		end
+
+		it "should handle one thousand to nine thousand nine hundred ninety-nine" do
+			1020.to_words.must_equal("one thousand twenty")
+		  1000.to_words.must_equal("one thousand")
+		  1001.to_words.must_equal("one thousand one")
+		  1011.to_words.must_equal("one thousand eleven")
+		  1078.to_words.must_equal("one thousand seventy-eight")
+		  1100.to_words.must_equal("one thousand one hundred")
+		  1216.to_words.must_equal("one thousand two hundred sixteen")
+		  1225.to_words.must_equal("one thousand two hundred twenty-five")
+		  2000.to_words.must_equal("two thousand")
+		  2356.to_words.must_equal("two thousand three hundred fifty-six")
+		  6863.to_words.must_equal("six thousand eight hundred sixty three")
+		  8864.to_words.must_equal("eight thousand eight hundred sixty four")
+		  9956.to_words.must_equal("nine thousand nine hundred fifty six")
+		end
+
+		# it "should handle ten thousand to ninety nine thousand nine hundred ninety-nine" do
+		# end
+
+		# it "should handle one hundred thousand to one million" do
+		# end
+
+	end
 end
